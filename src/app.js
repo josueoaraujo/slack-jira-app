ba7220ca20aa2217b90cf3ea256972cc94c417c1
Josué <josue.araujo@lightspeedhq.com>
2022-09-19 15:14:50 -0400

    Remove unnecessary comment

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const jiraApi = new JiraApi({
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // enable the following to use socket mode
+  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
